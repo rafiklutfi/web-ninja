4fadcd821e9c60d06bdc0bdb36e18778c7a68320
Rafik Lutfi <dev.rafiklutfi@gmail.com>
2021-03-23 18:26:56 +0700

    koneksi css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,21 @@
 import React from "react"
-import Navbar from "../components/Navbar"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import { header, btn } from "../styles/home.module.css"
 
 export default function Home() {
   return (
-    <section>
-      <Navbar />
-      <div>
-        <h2>Design</h2>
-        <h3>Develop & Deploy</h3>
-        <p> Web developer based in Batam</p>
-      </div>
-    </section>
+    <Layout>
+      <section className={header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>web developer based in Batam.</p>
+          <Link className={btn} to="/projects">
+            Portfolio Projects
+          </Link>
+        </div>
+      </section>
+    </Layout>
   )
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,9 +1,12 @@
 import React from "react"
+import Layout from "../../components/Layout"
 
 export default function index() {
   return (
-    <div>
-      <h2>Portofolio Kami</h2>
-    </div>
+    <Layout>
+      <div>
+        <h2>Portofolio Kami</h2>
+      </div>
+    </Layout>
   )
 }
